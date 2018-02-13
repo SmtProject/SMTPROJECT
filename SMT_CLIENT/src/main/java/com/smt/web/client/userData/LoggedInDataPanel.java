1f55e85cd8cc28ba8427daf8860fab7532ab9709
majoudibaltagi <majd.bal@gmail.com>
2018-02-13 21:02:11 +0200

    commit coloring

--- SMT_CLIENT/src/main/java/com/smt/web/client/userData/LoggedInDataPanel.java
+++ SMT_CLIENT/src/main/java/com/smt/web/client/userData/LoggedInDataPanel.java
@@ -1,7 +1,11 @@
 package com.smt.web.client.userData;
 
+import com.smt.data.entity.Admin;
 import com.smt.data.entity.SmtUser;
+import com.smt.data.entity.Teacher;
 import com.smt.web.client.loginPanel.MainUi;
+import com.vaadin.server.Page;
+import com.vaadin.server.Page.Styles;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
@@ -9,14 +13,15 @@ import com.vaadin.ui.UI;
 
 public class LoggedInDataPanel extends Panel{
 	private static final long serialVersionUID = -7253304854580709082L;
-	
+
 	private SmtUser user;
-	
+
 	public LoggedInDataPanel() {
 		user=((MainUi)UI.getCurrent()).getSmtUser();
 		this.setSizeFull();
 		initPanelCaption();
 		initLayout();
+		setBackGroundColor(user);
 	}
 
 	private void initLayout() {
@@ -33,5 +38,16 @@ public class LoggedInDataPanel extends Panel{
 	private void initPanelCaption() {
 		this.setCaption("Logged in as  "+user.getSmtRole().toString());	
 	}
-	
+
+	private void setBackGroundColor(SmtUser user) {
+		String colorName="grey";
+		if(user instanceof Admin)
+			colorName="#ffcccc";
+		if(user instanceof Teacher)
+			colorName="#e6f7ff";//teacher ==>#e6ffe6
+		this.setStyleName("backColorCustom");		
+		Styles styles = Page.getCurrent().getStyles();
+		styles.add("div.v-panel.v-widget.backColorCustom.v-panel-backColorCustom.v-has-width{background-color: "+colorName+";}");		
+	}
+
 }
