cf34871a00bddd3bb7ef7af7a3d9bb7b5e71f589
majoudibaltagi <majd.bal@gmail.com>
2018-04-28 12:00:29 +0300

    commmit test

--- SMT_MODEL/src/main/java/com/smt/application/service/SmtPaymentService.java
+++ SMT_MODEL/src/main/java/com/smt/application/service/SmtPaymentService.java
@@ -5,14 +5,21 @@ import java.util.List;
 import javax.xml.bind.ValidationException;
 
 import com.smt.data.entity.Payment;
+import com.smt.data.entity.PaymentDetail;
 
 public interface SmtPaymentService {
 
 	List<Payment> findAllStudentsPayment();
+	
+	public List<PaymentDetail>getPaymentDeatails(Integer paymentId)throws ValidationException;
 
 	Payment saveStudentPayment(Payment payment) throws ValidationException;
 
 	void saveStudentsPayment(List<Payment> studentsPayment) throws ValidationException;
 
 	public Payment findPaymentById(Integer id);
+	
+	public PaymentDetail findPaymentDetailsById(Integer id) throws ValidationException;
+	
+	public PaymentDetail updatePaymentDetail(PaymentDetail paymentDetail)throws ValidationException;
 }
